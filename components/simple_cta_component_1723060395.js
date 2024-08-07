9784c8fb5556db22dcc6a84a0bebce3c9f1ae530
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 19:56:21 +0000

    Code generation

--- components/simple_cta_component_1723060395.js
+++ components/simple_cta_component_1723060395.js
@@ -19,7 +19,7 @@ Vue.component("simple_cta_component_1723060395", {
                 </div>
 
                 <div id="hello-world-button-container" class="flex">
-                    <a id="hello-world-button" href="#" class="flex-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Hello World</a>
+                    <a id="hello-world-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Hello World</a>
                 </div>
             </div>
         </div>
